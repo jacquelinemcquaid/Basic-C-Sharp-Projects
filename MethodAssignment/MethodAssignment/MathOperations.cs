ea75ad3709a7faac1b58a03cfb74e385c83643e8
Jacqueline McQuaid <67380308+jacquelinemcquaid@users.noreply.github.com>
2020-08-12 13:20:00 -0300

    Add files via upload

---
+++ MethodAssignment/MethodAssignment/MathOperations.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    public class MathOperations
+    {
+        
+        public void Method1()
+        {
+            Console.WriteLine("Pick a number.");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("You number added to itself equals " + (number1 + number1) );
+            
+            
+        }
+
+        public void Method2()
+
+        {
+            Console.WriteLine("Pick a number.");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your number divided by 10 equals " + (number2 / 10));
+        }
+
+        public void Method3()
+        {
+            Console.WriteLine("Pick a number.");
+            int number3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("You number multiplied by itself = " + (number3 * number3));
+        }
+
+
+    }
+}
---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            MathOperations MathOperations = new MathOperations();
+
+            MathOperations.Method1();
+
+            MathOperations.Method2();
+
+            MathOperations.Method3();
+
+
+            Console.ReadLine();
+        }
+    }
+}
