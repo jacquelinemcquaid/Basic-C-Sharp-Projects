635a7d7c1aa143c409286c411cdbb22e74a8590d
Jacqueline McQuaid <67380308+jacquelinemcquaid@users.noreply.github.com>
2020-08-13 10:34:22 -0300

    Add files via upload

---
+++ ClassesAndMethodsAssignment2/ClassesAndMethodsAssignment2/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ClassesAndMethodsAssignment2
+{
+    class Class1
+    {
+       static public void Method1(int number1, [Optional] int number2)
+        {
+            Console.WriteLine(number1 * number2);
+        
+        }
+
+        static public void Method2(int num1, [Optional] int num2)
+        {
+           
+         
+            Console.WriteLine(num1 + num2);
+
+            
+
+        }
+    }
+}
---
+++ ClassesAndMethodsAssignment2/ClassesAndMethodsAssignment2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Class1 Class1 = new Class1();
+            //Task 1
+            Class1.Method1(5);
+            Class1.Method1(6, 8);
+
+            //Task 2
+            Console.WriteLine("Enter either one number, or two numbers (one at a time).");
+            Class1.Method2(Convert.ToInt32(Console.ReadLine()));
+            Class1.Method2(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+
+
+            Console.ReadLine();
+        }
+    }
+}
