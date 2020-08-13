88bc4de15831ec6cd93b555bcf7c62b1ac59154d
Jacqueline McQuaid <67380308+jacquelinemcquaid@users.noreply.github.com>
2020-08-13 12:20:09 -0300

    Add files via upload

---
+++ ClassMethodAssignement/ClassMethodAssignement/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignement
+{
+    class Class1
+    {
+        public void Method1(int num1)
+        {
+            Console.WriteLine($"{num1} / 2 = " + (num1 / 2));
+        }
+
+        public void Method2(string firstName, string lastName)
+        {
+            Console.WriteLine("My name is " + firstName + " " + lastName);
+        }
+
+        public static void Overloading(int a, int b)
+        {
+            Console.WriteLine($" {a} + {b} = " + (a + b));
+            
+        }
+
+        public static void Overloading(string c, string d)
+        {
+            Console.WriteLine(" My favourit food is " + c + ", and my favourite colour is " + d);
+        }
+
+    }
+}
---
+++ ClassMethodAssignement/ClassMethodAssignement/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1
+            Class1 Class1 = new Class1();
+            Console.WriteLine("Pick a number.");
+            Class1.Method1(Convert.ToInt32(Console.ReadLine()));
+
+            //Task 2
+            Console.WriteLine("Enter your first and last name one at a time.");
+            Class1.Method2(Console.ReadLine(), Console.ReadLine());
+
+            //Task 3 and 4
+            Console.WriteLine("Enter two numbers, one at a time.");
+            Class1.Overloading(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("Enter your favourite food. Then enter your favourite colour.");
+            Class1.Overloading(Console.ReadLine(), Console.ReadLine());
+
+            Console.ReadLine();
+
+        }
+    }
+}
