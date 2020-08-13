9ddbdf57c6c2534fce93517a9b6f33ea4f0815cf
Jacqueline McQuaid <67380308+jacquelinemcquaid@users.noreply.github.com>
2020-08-13 10:57:36 -0300

    Add files via upload

---
+++ ClassesAndMethodsAssignment3/ClassesAndMethodsAssignment3/New.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsAssignment3
+{
+    class New
+    {
+        public void Method1(int num1, int num2)
+        {
+
+            Console.WriteLine($"{num1} x {num1} = " + (num1 * num1));
+                
+            Console.WriteLine($"{num2} is " + num2);
+
+            Console.WriteLine($"Parameter 1 = {num1}, Parameter 2 = {num2}");
+
+
+        }
+
+    }
+}
---
+++ ClassesAndMethodsAssignment3/ClassesAndMethodsAssignment3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            New New = new New();
+
+            Console.WriteLine("Pick two numbers, one at a time.");
+            New.Method1(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+            Console.ReadLine();
+        }
+    }
+}
