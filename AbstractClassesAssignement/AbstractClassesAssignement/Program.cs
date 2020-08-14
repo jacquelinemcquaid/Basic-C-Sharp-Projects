607c82e17f6a577a401194ace3e1fd955b10c22f
Jacqueline McQuaid <67380308+jacquelinemcquaid@users.noreply.github.com>
2020-08-14 09:31:42 -0300

    Add files via upload

---
+++ AbstractClassesAssignement/AbstractClassesAssignement/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesAssignement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Employee = new Employee();
+
+            Employee.firstName = "Sample";
+            Employee.lastName = "Student";
+
+            Employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
