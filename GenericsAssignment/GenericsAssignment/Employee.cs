9859c5d5b44960259a52952d91b0dfbb560306da
Jacqueline McQuaid <67380308+jacquelinemcquaid@users.noreply.github.com>
2020-08-14 16:15:20 -0300

    Add files via upload

---
+++ GenericsAssignment/GenericsAssignment/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAssignment
+{
+    public class Employee<T> : Person
+    {
+        public T Number { get; set; }
+      
+
+    }
+}
+
+
---
+++ GenericsAssignment/GenericsAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GenericsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> count = new Employee<string>();
+            count.Number = "500";
+
+            Console.WriteLine("There are " + count.Number + " employees.");
+            Console.ReadLine();
+
+            Person Person = new Person();
+
+            Person.firstName = "Sample";
+            Person.lastName = "Student";
+
+            Person.SayName();
+            Console.ReadLine();
+
+        }
+    }
+}
