725d55091bc3de212d87ea597a7a67be2189d57c
Jacqueline McQuaid <67380308+jacquelinemcquaid@users.noreply.github.com>
2020-08-14 10:52:55 -0300

    Add files via upload

---
+++ InterfacesAssignment/InterfacesAssignment/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesAssignment
+{
+    public class Employee : Person, IQuittable
+    {
+
+        
+       public void Quit()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine(name + " wants to quit his/her job. Type 'okay' to continue.");
+            Console.ReadLine();
+            
+        }
+
+        public void notice()
+        {
+            Console.WriteLine("How many weeks notice are you giving?");
+            int weeks = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("He/she is giving " + weeks + " weeks notice.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ InterfacesAssignment/InterfacesAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Employee = new Employee();
+
+            Employee.Quit();
+            Employee.notice();
+
+        }
+    }
+}
