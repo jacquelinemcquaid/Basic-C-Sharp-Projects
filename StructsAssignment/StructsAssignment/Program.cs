75cc8e5369c5623ba911e40d825497d238b635bd
Jacqueline McQuaid <67380308+jacquelinemcquaid@users.noreply.github.com>
2020-08-17 11:07:01 -0300

    Add files via upload

---
+++ StructsAssignment/StructsAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number();
+
+            num.Amount = 5.67m;
+
+            Console.WriteLine(num.Amount);
+            Console.ReadLine();
+        }
+    }
+}
