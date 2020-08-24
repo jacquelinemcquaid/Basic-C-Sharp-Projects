1cb087b56dad1201c4750e2431f13f15223995a3
Jacqueline McQuaid <67380308+jacquelinemcquaid@users.noreply.github.com>
2020-08-24 10:50:51 -0300

    Add files via upload

---
+++ AdditionalFeaturesAssignment/AdditionalFeaturesAssignment/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeaturesAssignment
+{
+    public class Employee
+    {
+        public string firstName;
+        public string lastName;
+        public string empID;
+        public string position;
+        public string team;
+        public Employee() : this("firstName", "lastName")
+        {
+            Console.WriteLine("We have wonderful employees working for our company.");
+        }
+        public Employee(string firstName) : this(firstName, "lastName")
+        {
+            this.firstName = firstName;
+            Console.WriteLine("The first name of our 'Employee of the month!'");
+        }
+        public Employee(string firstName, string lastName) : this(firstName, lastName, "2760", "Manager", "Software Developer")
+        {
+            this.lastName = lastName;
+            this.firstName = firstName;
+            Console.WriteLine("The first and last names of our 'Employee of the month!'");
+        }
+        public Employee(string firstName, string lastName, string empID, string position, string team)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.empID = empID;
+            this.position = position;
+            this.team = team;
+            Console.WriteLine("The first name, last name, empID, position and team for our 'Employee of the month!'");
+        }
+
+        public void printName()
+        {
+            Console.WriteLine(firstName + " " + lastName + " " + empID + " " + position + " " + team + ".");
+        }
+    }
+}
+
---
+++ AdditionalFeaturesAssignment/AdditionalFeaturesAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeaturesAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const string firstName = "Jacqueline";
+
+            Console.WriteLine("What is your first name?");
+            Console.WriteLine(firstName);
+            Console.ReadLine();
+
+            var favouriteAnimal = "Dog";
+
+            Console.WriteLine("What is your favourite animal?");
+            Console.WriteLine(favouriteAnimal);
+            Console.ReadLine();
+
+            Employee employee = new Employee("Fiona", "Rogers");
+            employee.printName();
+
+            Console.ReadLine();
+
+        }
+    }
+}
